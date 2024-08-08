d58e50a4c6f4d7f0fc2d44426204e1595ed55129
siammahamud <siammahamud710@gmail.com>
2024-08-08 19:43:47 +0600

    top panel of header has been added including logo,search bar and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      colors:{
+        'primary':'#F0F2F3',
+        'btn-color':'#029FAE'
+  
+        
+      }
+
     },
   },
   plugins: [],
