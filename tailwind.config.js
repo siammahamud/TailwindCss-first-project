40848c973857cd0ceade18eca7dda542844cbefc
siammahamud <siammahamud710@gmail.com>
2024-08-09 11:21:05 +0600

    nav link section of header is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,28 @@ module.exports = {
     extend: {
       colors:{
         'primary':'#F0F2F3',
-        'btn-color':'#029FAE'
-  
-        
+        'btn-color':'#029FAE',
+        'txt-light':'#636270'
+      },
+      keyframes: {
+        underlineIn: {
+          '0%': { transform: 'scaleX(0)' },
+          '100%': { transform: 'scaleX(1)' },
+        },
+        underlineOut: {
+          '0%': { transform: 'scaleX(1)' },
+          '100%': { transform: 'scaleX(0)' },
+        },
+      },
+      animation: {
+        underlineIn: 'underlineIn 0.3s ease-out',
+        underlineOut: 'underlineOut 0.3s ease-in',
       }
-
+    
     },
+
+    
   },
-  plugins: [],
+
+  plugins: []
 }
