b447b6dba25cce035ad1c8ac8ffa6a7d965ed118
siammahamud <siammahamud710@gmail.com>
2024-08-09 11:52:40 +0600

    nav link hover effect added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,6 @@ module.exports = {
         'btn-color':'#029FAE',
         'txt-light':'#636270'
       },
-      keyframes: {
-        underlineIn: {
-          '0%': { transform: 'scaleX(0)' },
-          '100%': { transform: 'scaleX(1)' },
-        },
-        underlineOut: {
-          '0%': { transform: 'scaleX(1)' },
-          '100%': { transform: 'scaleX(0)' },
-        },
-      },
-      animation: {
-        underlineIn: 'underlineIn 0.3s ease-out',
-        underlineOut: 'underlineOut 0.3s ease-in',
-      }
     
     },
 
