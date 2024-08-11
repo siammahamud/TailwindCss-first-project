c18e5e528d9e5defe39358aa4ea0834c4024f707
siammahamud <siammahamud710@gmail.com>
2024-08-11 17:53:13 +0600

    Review section and recent products added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,24 @@ module.exports = {
         inter: ['Inter', 'sans-serif'],
       },  
     
-    },
 
-    
+    keyframes: {
+      'border-animate': {
+        '0%': { borderTopColor: 'cyan', borderRightColor: 'transparent', borderBottomColor: 'transparent', borderLeftColor: 'transparent' },
+        '25%': { borderTopColor: 'cyan', borderRightColor: 'cyan', borderBottomColor: 'transparent', borderLeftColor: 'transparent' },
+        '50%': { borderTopColor: 'cyan', borderRightColor: 'cyan', borderBottomColor: 'cyan', borderLeftColor: 'transparent' },
+        '75%': { borderTopColor: 'cyan', borderRightColor: 'cyan', borderBottomColor: 'cyan', borderLeftColor: 'cyan' },
+        '100%': { borderTopColor: 'transparent', borderRightColor: 'transparent', borderBottomColor: 'transparent', borderLeftColor: 'cyan' },
+      },
+    },
+    animation: {
+      'border-animate': 'border-animate 2s linear infinite',
+    },
   },
 
+    
+},
+
   plugins: []
+
 }
